b178ee6c4c07f3b1cc60d296d805525fad37f035
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-03-01 15:15:39 -0600

    Adding Body Parser

--- server.js
+++ server.js
@@ -2,10 +2,53 @@ const express = require('express');
 
 const app = express();
 
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+			{
+			id: '124',
+			name: 'Sally',
+			email: 'sally@gmail.com',
+			password: 'bananas',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
 app.get('/', (req, res) => {
 	res.send('this idfs working');
+})
+
+app.post('/signin', (req, res) => {
+	if (req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password)
+		{
+			res.json('success')
+		} else {
+			res.status(400).json('error logging in');
+		}
+	res.json("signin")
 })
 
 app.listen(3000, () => {
 	console.log('app is running yo on port 3000');
 } )
+
+
+/* What do we want for our Restful API?
+
+/ --> res = this is working
+/signin route --> POST (post data) responds with either success/fail
+/resigser --> POST = new user object
+/profile/:userId --> GET = user
+/Image --> PUT (since the user already exists) --> user
+
+*/
